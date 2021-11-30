d62cc384afd7b914919e58cd24260a43c67f41be
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-11-30 18:02:03 +0000

    fix: env variable

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,8 @@
+require('dotenv').config()
+const pword = process.env.PWORD
+
 export default {
     port: 27017,
     host: 'localhost',
-    dbUri: 'mongodb+srv://shlomoLiquid:LiquidLiquid@thebackend.laolw.mongodb.net/theBackEnd?retryWrites=true&w=majority',
+    dbUri: `mongodb+srv://shlomoLiquid:${pword}@thebackend.laolw.mongodb.net/theBackEnd?retryWrites=true&w=majority`,
 }
